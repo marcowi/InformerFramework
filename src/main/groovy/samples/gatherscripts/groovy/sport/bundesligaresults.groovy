8774436baede7b2694a75d08084d5ba32662afe7
marco <marco@code-xy.com>
2018-02-25 21:14:36 +0100

    Simplified bundesliga gather script

--- src/main/groovy/samples/gatherscripts/groovy/sport/bundesligaresults.groovy
+++ src/main/groovy/samples/gatherscripts/groovy/sport/bundesligaresults.groovy
@@ -8,11 +8,28 @@ static def gather(URL url) {
 
 
     def apiResponse = new JsonSlurper().parseText(url.text)
+    def simplifiedResults = simplifyResults(apiResponse)
 
     def e = [:]
     e.id = new Date().time
-    e.content = apiResponse
+    e.content = simplifiedResults
 
     return new JsonBuilder([e]).toString()
 
+}
+
+static def simplifyResults(def results) {
+    def simplifiedResults = []
+
+    results.each { result ->
+        def match = [:]
+        match.date = result.MatchDateTime
+        match.group = result.Group.GroupName
+        match.team1 = [name: result.Team1.TeamName, points: result.MatchResults.collect{[(it.ResultName): it.PointsTeam1]}]
+        match.team2 = [name: result.Team2.TeamName, points: result.MatchResults.collect{[(it.ResultName): it.PointsTeam2]}]
+        simplifiedResults << match
+    }
+
+
+    return simplifiedResults
 }
